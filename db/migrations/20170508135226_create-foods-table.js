80d336089782b7500381054cc064eada92067320
blackknight75 <devbox4714@gmail.com>
2017-05-08 14:10:59 -0600

    add database to app. database not fully connected yet.

---
+++ db/migrations/20170508135226_create-foods-table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  let createQuery = `CREATE TABLE food (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name TEXT,
+    calories INTEGER,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+  )`
+  return knex.raw(createQuery);
+};
+
+exports.down = function(knex, Promise) {
+  let dropQuery = `DROP TABLE food`;
+  return knex.raw(dropQuery);
+};
---
+++ db/seeds/dev/foods.js
@@ -0,0 +1,20 @@
+
+exports.seed = function(knex, Promise) {
+  return knex.raw('TRUNCATE food RESTART IDENTITY')
+  .then(() => {
+    return Promise.all([
+      knex.raw(
+        'INSERT INTO food (name, calories, created_at, updated_at) VALUES (?, ?, ?, ?)',
+        ["Banana", 100, new Date, new Date]
+      ),
+      knex.raw(
+        'INSERT INTO food (name, calories, created_at, updated_at) VALUES (?, ?, ?, ?)',
+        ["Apple", 50, new Date, new Date]
+      ),
+      knex.raw(
+        'INSERT INTO food (name, calories, created_at, updated_at) VALUES (?, ?, ?, ?)',
+        ["Steak", 500, new Date, new Date]
+      )
+    ]);
+  });
+};
